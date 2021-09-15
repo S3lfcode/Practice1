13b8fdf136f8677117b000053dffdbf00613b823
M3d1vh <aeti0sxd@gmail.com>
2021-09-15 11:57:29 +0300

    commit 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		System.out.println("For develop commit 1.1");
+		System.out.println("For develop commit 2.1");
+		System.out.println("For develop commit 3.1");
+		System.out.println("For develop commit 4.1");
+		System.out.println("For develop commit 5.1");
 	}
 }
